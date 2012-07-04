ca0b8b003bf5ed9af332e2e0d2bc4f7aeff1728f
Luis Vega <luiscvega@gmail.com>
2012-07-04 13:33:30 +0800

    changed

--- controller.rb
+++ controller.rb
@@ -1,7 +1,41 @@
-layout 'layout.html.erb'
+require 'yaml'
 
 helpers do
   def link_to(url, text)
-    "<a href=\"#{url}\">#{text}</a>"
+    "<a href=\"#{url}.html\">#{text}</a>"
   end
 end
+
+ignore 'meat.html.erb'
+layout 'layout.html.erb'
+layout 'meat.html.erb' => 'layout.html.erb'
+
+def generate(name, variants)
+  before "#{name.downcase}.html.erb" do
+    instead render 'meat.html.erb', 
+      locals: {
+        meat: name,
+        variants: variants
+      }
+  end
+end
+
+meats = YAML::load(File.open('meats.yml'))
+
+meats.each do |meat, variants|
+  generate meat, variants
+end
+
+# generate "Pork",
+  # [
+  #   { 
+  #     name: "Tenderloin",
+  #     img: "http://placehold.it/300x150",
+  #     description: "Rib-Eye is delicious."
+  #   }, 
+  #   { 
+  #     name: "Rib-Eye",
+  #     img: "http://placehold.it/300x150",
+  #     description: "Rib-Eye is delicious."
+  #   },
+  # ]
